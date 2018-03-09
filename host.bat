5caece9ed41596b60aa9e269d97671b9fa7d4821
coding-dream <352085768@qq.com>
2018-03-09 16:39:45 +0800

    update

---
+++ host.bat
@@ -0,0 +1,21 @@
+@echo off
+c:
+cd %SystemRoot%\system32\drivers\etc\
+
+echo. >> hosts
+echo 210.129.120.55 www.pixiv.net >> hosts
+echo 210.129.120.43 touch.pixiv.net >> hosts
+echo 210.140.131.146 source.pixiv.net >> hosts
+echo 210.129.120.41 accounts.pixiv.net >> hosts
+echo 210.140.131.147 imgaz.pixiv.net >> hosts
+echo 210.129.120.44 app-api.pixiv.net >> hosts
+echo 210.129.120.48 oauth.secure.pixiv.net >> hosts
+echo 210.129.120.54 dic.pixiv.net >> hosts
+echo 210.140.131.145 comic.pixiv.net >> hosts
+echo 210.129.120.53 factory.pixiv.net >> hosts
+echo 74.120.148.205 g-client-proxy.pixiv.net >> hosts
+echo 210.140.170.179 sketch.pixiv.net >> hosts
+echo 210.129.120.52 payment.pixiv.net >> hosts
+echo 210.129.120.51 sensei.pixiv.net >> hosts
+echo 210.140.131.144 novel.pixiv.net >> hosts
+echo 210.129.120.49 ssl.pixiv.net >> hosts
