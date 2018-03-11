f53b4cc7c5d9eebadc4b52e713529ec4085ee144
coding-dream <352085768@qq.com>
2018-03-11 10:24:05 +0800

    update blogs

--- source/apps/db/publish.bat
+++ source/apps/db/publish.bat
@@ -1,6 +1,4 @@
 @echo off
-
-echo ========= update json =========
-git status && git add . && git commit -m "update blogs" && hexo clean && hexo g && hexo d
+hexo clean && hexo g && hexo d
 pause
 exit
